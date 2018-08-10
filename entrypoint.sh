ce0287e205388ed914ce59c3d0098af40acf1c5f
Jirka Kremser <jkremser@redhat.com>
2018-08-10 15:13:13 +0200

    using entrypoint.sh to overcome the no passwd record

---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Check whether there is a passwd entry for the container UID
+myuid=$(id -u)
+mygid=$(id -g)
+
+# turn off -e for getent because it will return error code in anonymous uid case
+set +e
+uidentry=$(getent passwd $myuid)
+set -e
+
+# If there is no passwd entry for the container UID, attempt to create one
+if [ -z "$uidentry" ] ; then
+    if [ -w /etc/passwd ] ; then
+        echo "$myuid:x:$myuid:$mygid:anonymous uid:$SPARK_HOME:/bin/false" >> /etc/passwd
+    else
+        echo "Container ENTRYPOINT failed to add passwd entry for anonymous UID"
+    fi
+fi
+
+$@
